aca2b3450e2750fa1fea9626031c818d03f7ce28
Jakub Kwiatkowski <44145413+panjacob@users.noreply.github.com>
2022-05-13 21:14:24 +0200

    training on amd gpu

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 import os
 
-os.environ["KERAS_BACKEND"] = "plaidml.bridge.keras"
-import keras
-# KERAS_BACKEND=plaidml.keras.backend
+import sys
+# AMD GPU
+if sys.version.split(' ')[0] == "3.8.6":
+    os.environ["KERAS_BACKEND"] = "plaidml.bridge.keras"
+    import plaidml.keras
+
+    plaidml.keras.install_backend()
+
 from generator import get_generators
 from model import get_model
 
-train_generator, test_generator = get_generators("testy", is_create_new_dataset=True, limit_count=True)
+train_generator, test_generator = get_generators("testy", is_create_new_dataset=True, limit_count=False)
 model = get_model()
 model.fit_generator(
     train_generator,
-    steps_per_epoch=2000,
-    epochs=50,
+    steps_per_epoch=100,
+    epochs=2,
     validation_data=test_generator,
     validation_steps=800)
 model.save_weights('first_try.h5')
--- model.py
+++ model.py
@@ -1,5 +1,3 @@
-import os
-from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense
@@ -7,7 +5,7 @@ from keras.layers import Activation, Dropout, Flatten, Dense
 
 def get_model():
     model = Sequential()
-    model.add(Conv2D(32, (3, 3), input_shape=(256, 256,3)))
+    model.add(Conv2D(32, (3, 3), input_shape=(256, 256, 3)))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2), padding='same'))
 
@@ -23,10 +21,11 @@ def get_model():
     model.add(Dense(64))
     model.add(Activation('relu'))
     model.add(Dropout(0.5))
-    model.add(Dense(1))
+    model.add(Dense(4))
     model.add(Activation('sigmoid'))
 
-    model.compile(loss='binary_crossentropy',
-                  optimizer='rmsprop',
-                  metrics=['accuracy'])
+    model.compile(
+        loss='binary_crossentropy',
+        optimizer='rmsprop',
+        metrics=['accuracy'])
     return model
